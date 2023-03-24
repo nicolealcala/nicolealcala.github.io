1f239f39fa69c5a30d78f01083a7014700ca9ba3
nicolealcala <nicolealcalq@gmail.com>
2023-03-25 00:37:29 +0800

    upload assets

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
-  $("#menu-check").click(
-    function () {
-      $(".menuPage").removeClass("d-none");
-      $("section").not('.menuPage').addClass("d-none");
-    },
+  // $("#menu-check").click(
+  //   function () {
+  //     $(".menuPage").removeClass("d-none");
+  //     $("section").not('.menuPage').addClass("d-none");
+  //   },
 
-    function () {
-      $(".menuPage").addClass("d-none");
-      $("section").not('.menuPage').removeClass("d-none");
-    }
-  );
+  //   function () {
+  //     $(".menuPage").addClass("d-none");
+  //     $("section").not('.menuPage').removeClass("d-none");
+  //   }
+  // );
+
+  $('#allWorks').click(function(){
+    $(this).addClass('active');
+    $('.navBtn').not(this).removeClass('active');
+  })
+
+  $('#websites').click(function(){
+    $(this).addClass('active');
+    $('.navBtn').not(this).removeClass('active');
+  })
+
+  $('#branding').click(function(){
+    $(this).addClass('active');
+    $('.navBtn').not(this).removeClass('active');
+  })
 });
