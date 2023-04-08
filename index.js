1597f7d1bb9d23bfc86a25a242cdaf7229e7d2e7
nicolealcala <nicolealcalq@gmail.com>
2023-04-08 17:26:15 +0800

    navigation

--- index.js
+++ index.js
@@ -1,4 +1,118 @@
 $(document).ready(function () {
+  //Menu Page
+  $(".menu-label").click(function () {
+    $("#menu-check").on("change", function () {
+      if ($(this).is(":checked")) {
+        $(".line1").css({
+          width: "70px",
+          margin: "0 2px 0 0",
+          transform: "rotate(45deg) scaleX(0.55) translate(45px, -4.5px)",
+          "border-radius": "50px 50px 50px 0",
+        });
+        $(".line2 ").css({
+          "border-top-right-radius": "50px",
+          "border-bottom-right-radius": "50px",
+          width: "0px",
+        });
+        $(".line3").css({
+          width: "70px",
+          margin: "27px 0 0 0",
+          transform: "rotate(-45deg) scaleX(0.55) translate(40px, 4.5px)",
+          "border-radius": "0 50px 50px 50px",
+        });
+        $(".menuPage").removeClass("d-none");
+        $("section").not(".menuPage").addClass("d-none");
+
+        // Links
+        $("#link-home").click(function () {
+          $(this).removeClass("stroked").addClass("nav-active");
+          $(".navTxt").not(this).removeClass("nav-active").addClass("stroked");
+          $("#landing").removeClass("d-none").addClass("active");
+          $("section").not("#landing").removeClass("active").addClass("d-none");
+          $(".line1, .line2, .line3").css({
+            width: "70px",
+            height: "2px",
+            "background-color": "#fff",
+            position: "absolute",
+            transform: "rotate(0deg) scaleX(1) translate(0, 0)",
+          });
+          $(".line2").css("width", "50px");
+          $(".line3").css("margin", "36px 0 0 0");
+        });
+        $("#link-about").click(function () {
+          $(this).removeClass("stroked").addClass("nav-active");
+          $(".navTxt").not(this).removeClass("nav-active").addClass("stroked");
+          $("#about").removeClass("d-none").addClass("active");
+          $("section").not("#about").removeClass("active").addClass("d-none");
+          $(".line1, .line2, .line3").css({
+            width: "70px",
+            height: "2px",
+            "background-color": "#fff",
+            position: "absolute",
+            transform: "rotate(0deg) scaleX(1) translate(0, 0)",
+          });
+          $(".line2").css("width", "50px");
+          $(".line3").css("margin", "36px 0 0 0");
+        });
+        $("#link-portfolio").click(function () {
+          $(this).removeClass("stroked").addClass("nav-active");
+          $(".navTxt").not(this).removeClass("nav-active").addClass("stroked");
+          $("#portfolio").removeClass("d-none").addClass("active");
+          $("section")
+            .not("#portfolio")
+            .removeClass("active")
+            .addClass("d-none");
+          $(".line1, .line2, .line3").css({
+            width: "70px",
+            height: "2px",
+            "background-color": "#fff",
+            position: "absolute",
+            transform: "rotate(0deg) scaleX(1) translate(0, 0)",
+          });
+          $(".line2").css("width", "50px");
+          $(".line3").css("margin", "36px 0 0 0");
+        });
+        $("#link-contact").click(function () {
+          $(this).removeClass("stroked").addClass("nav-active");
+          $(".navTxt").not(this).removeClass("nav-active").addClass("stroked");
+          $("#contact").removeClass("d-none").addClass("active");
+          $("section").not("#contact").removeClass("active").addClass("d-none");
+          $(".line1, .line2, .line3").css({
+            width: "70px",
+            height: "2px",
+            "background-color": "#fff",
+            position: "absolute",
+            transform: "rotate(0deg) scaleX(1) translate(0, 0)",
+          });
+          $(".line2").css("width", "50px");
+          $(".line3").css("margin", "36px 0 0 0");
+        });
+      } else {
+        $(".line1, .line2, .line3").css({
+          width: "70px",
+          height: "2px",
+          "background-color": "#fff",
+          margin: "0",
+          position: "absolute",
+          transform: "rotate(0deg) scaleX(1) translate(0, 0)",
+        });
+        $(".line2").css({ width: "50px", margin: "18px 0 0 0" });
+        $(".line3").css("margin", "36px 0 0 0");
+        setTimeout(function () {
+          $(".menuPage")
+            .removeClass("animate__slideInDown")
+            .addClass("animate__slideOutUp");
+        }, 3000);
+        // $(".menuPage").addClass("d-none");
+        $("section.active").removeClass("d-none");
+      }
+    });
+  });
+
+  $("#learnMore").click(function () {
+    $("section").not("#about").removeClass("active").addClass("d-none");
+  });
+
   // Animations
   var bodyBgImage = new Image();
   bodyBgImage.onload = function () {
@@ -8,90 +122,124 @@ $(document).ready(function () {
   bodyBgImage.src =
     "https://github.com/nicolealcala/Portfolio/blob/main/assets/background01.png?raw=true";
 
-  //Entrance and Exit animations on scroll
-  // Function to add exit animation to elements in the current section
-  function addExitAnimationToCurrentSection() {
-    var currentSection = $("section.active");
-    currentSection
-      .find(".animate__animated.animate__slideInDown")
-      .removeClass("animate__slideInDown")
-      .addClass("animate__fadeOutUp");
-    currentSection
-      .find(".animate__animated.animate__slideInUp")
-      .removeClass("animate__slideInUp")
-      .addClass("animate__fadeOutDown");
-    currentSection
-      .find(".animate__animated.animate__slideInLeft")
-      .removeClass("animate__slideInLeft")
-      .addClass("animate__fadeOutLeft");
-    currentSection
-      .find(".animate__animated.animate__slideInRight")
-      .removeClass("animate__slideInRight")
-      .addClass("animate__fadeOutRight");
-  }
+  // //Entrance and Exit animations on scroll
+  // // Function to add exit animation to elements in the current section
+  // function addExitAnimationToCurrentSection() {
+  //   // var currentSection = $("section.active");
+  // }
 
-  // Function to add entrance animation to elements in the next section
-  function addEntranceAnimationToNextSection() {
-    var nextSection = $("section.active").next("section");
-    nextSection
-      .find(".animate__animated.animate__slideInDown")
-      .removeClass("animate__slideInDown")
-      .addClass("animate__fadeOutUp");
-    nextSection
-      .find(".animate__animated.animate__slideInUp")
-      .removeClass("animate__slideInUp")
-      .addClass("animate__fadeOutDown");
-    nextSection
-      .find(".animate__animated.animate__slideInLeft")
-      .removeClass("animate__slideInLeft")
-      .addClass("animate__fadeOutLeft");
-    nextSection
-      .find(".animate__animated.animate__slideInRight")
-      .removeClass("animate__slideInRight")
-      .addClass("animate__fadeOutRight");
-  }
+  // // Function to add entrance animation to elements in the next section
+  // function addEntranceAnimationToNextSection() {
+  //   var nextSection = $("section.active").next("section");
+  // }
 
-  // Function to add entrance animation to elements in the previous section
-  function addEntranceAnimationToPreviousSection() {
-    var prevSection = $("section.active").prev("section");
-    prevSection
-      .find(".animate__animated.animate__slideInDown")
-      .removeClass("animate__slideInDown")
-      .addClass("animate__fadeOutUp");
-    prevSection
-      .find(".animate__animated.animate__slideInUp")
-      .removeClass("animate__slideInUp")
-      .addClass("animate__fadeOutDown");
-    prevSection
-      .find(".animate__animated.animate__slideInLeft")
-      .removeClass("animate__slideInLeft")
-      .addClass("animate__fadeOutLeft");
-    prevSection
-      .find(".animate__animated.animate__slideInRight")
-      .removeClass("animate__slideInRight")
-      .addClass("animate__fadeOutRight");
-  }
+  // // Function to add entrance animation to elements in the previous section
+  // function addEntranceAnimationToPreviousSection() {
+  //   var prevSection = $("section.active").prev("section");
+  //   prevSection
+  //     .find(".animate__animated.animate__slideInDown")
+  //     .removeClass("animate__slideInDown")
+  //     .addClass("animate__fadeOutUp");
+  //   prevSection
+  //     .find(".animate__animated.animate__slideInUp")
+  //     .removeClass("animate__slideInUp")
+  //     .addClass("animate__fadeOutDown");
+  //   prevSection
+  //     .find(".animate__animated.animate__slideInLeft")
+  //     .removeClass("animate__slideInLeft")
+  //     .addClass("animate__fadeOutLeft");
+  //   prevSection
+  //     .find(".animate__animated.animate__slideInRight")
+  //     .removeClass("animate__slideInRight")
+  //     .addClass("animate__fadeOutRight");
+  // }
 
-  // Detect when the user scrolls
-  $(window).on("scroll", function () {
-    var currentSection = null;
-    $("section").each(function () {
-      var sectionTop = $(this).offset().top;
-      var sectionHeight = $(this).outerHeight();
-      var scrollTop = $(window).scrollTop();
-      if (scrollTop >= sectionTop && scrollTop < sectionTop + sectionHeight) {
-        currentSection = $(this);
-      }
-    });
+  // var currentSection; // Variable to keep track of current section
 
-    // Remove "active" class from all sections
-    $("section").removeClass("active");
+  // // Function to animate exit of current section
+  // function animateSectionExit(currentSection) {
+  //   currentSection
+  //     .find(".animate__animated.animate__slideInDown")
+  //     .removeClass("animate__slideInDown")
+  //     .addClass("animate__fadeOutUp");
+  //   currentSection
+  //     .find(".animate__animated.animate__slideInUp")
+  //     .removeClass("animate__slideInUp")
+  //     .addClass("animate__fadeOutDown");
+  //   currentSection
+  //     .find(".animate__animated.animate__slideInLeft")
+  //     .removeClass("animate__slideInLeft")
+  //     .addClass("animate__fadeOutLeft");
+  //   currentSection
+  //     .find(".animate__animated.animate__slideInRight")
+  //     .removeClass("animate__slideInRight")
+  //     .addClass("animate__fadeOutRight");
+  //   setTimeout(function () {
+  //     currentSection.find("animate__animated animate__fadeOutUp");
+  //     currentSection.removeClass("animate__fadeOutUp");
+  //     currentSection.find("animate__animated animate__fadeOutDown");
+  //     currentSection.removeClass("animate__fadeOutDown");
+  //     currentSection.find("animate__animated animate__fadeOutLeft");
+  //     currentSection.removeClass("animate__fadeOutLeft");
+  //     currentSection.find("animate__animated animate__fadeOutRight");
+  //     currentSection.removeClass("animate__fadeOutRight");
+  //     currentSection.hide();
+  //   }, 1000); // Add a delay to allow animation to complete
+  // }
 
-    // Add "active" class to the currently visible section
-    if (currentSection) {
-      currentSection.addClass("active");
-    }
-  });
+  // // Function to animate entrance of next section
+  // function animateSectionEntrance(nextSection) {
+  //   nextSection
+  //     .find(".animate__animated.animate__slideInDown")
+  //     .removeClass("animate__slideInDown")
+  //     .addClass("animate__fadeOutUp");
+  //   nextSection
+  //     .find(".animate__animated.animate__slideInUp")
+  //     .removeClass("animate__slideInUp")
+  //     .addClass("animate__fadeOutDown");
+  //   nextSection
+  //     .find(".animate__animated.animate__slideInLeft")
+  //     .removeClass("animate__slideInLeft")
+  //     .addClass("animate__fadeOutLeft");
+  //   nextSection
+  //     .find(".animate__animated.animate__slideInRight")
+  //     .removeClass("animate__slideInRight")
+  //     .addClass("animate__fadeOutRight");
+  //   nextSection.show();
+  //   setTimeout(function () {
+  //     nextSection.removeClass("animate__animated animate__fadeInDown");
+  //     currentSection.find("animate__animated animate__fadeOutUp");
+  //     nextSection.removeClass("animate__fadeOutUp");
+  //     nextSection.find("animate__animated animate__fadeOutDown");
+  //     nextSection.removeClass("animate__fadeOutDown");
+  //     nextSection.find("animate__animated animate__fadeOutLeft");
+  //     nextSection.removeClass("animate__fadeOutLeft");
+  //     nextSection.find("animate__animated animate__fadeOutRight");
+  //     nextSection.removeClass("animate__fadeOutRight");
+  //   }, 1000); // Add a delay to allow animation to complete
+  // }
+
+  // // Scroll event listener
+  // $(window).on("scroll", function () {
+  //   var scrollTop = $(window).scrollTop();
+  //   var windowHeight = $(window).height();
+
+  //   // Check if scrolled down
+  //   if (scrollTop > windowHeight) {
+  //     animateSectionExit(currentSection);
+  //     currentSection = currentSection.next("section"); // Update current section
+  //     animateSectionEntrance(currentSection);
+  //   }
+  //   // Check if scrolled up
+  //   else {
+  //     animateSectionExit(currentSection);
+  //     currentSection = currentSection.prev("section"); // Update current section
+  //     animateSectionEntrance(currentSection);
+  //   }
+  // });
+
+  // currentSection = $("#landing"); // Set initial current section as section A
+  // animateSectionEntrance(currentSection); // Show section A with entrance animation
 
   // SKILLS
   const counter90 = $(".counter90");
