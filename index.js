8d77d29a0893232f0a53f90bda5504f7bbf50bb7
nicolealcala <nicolealcalq@gmail.com>
2023-08-06 22:11:08 +0800

    back to top

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ $(document).ready(function () {
     $(window).on("load", function () {
         loader.hide(); // Hide the loader when the page has finished loading
     });
+
+    var landingPageHeight = $("#landingPage").height();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > landingPageHeight) {
+            $('#backToTop').fadeIn();
+        } else {
+            $('#backToTop').fadeOut();
+        }
+    });
+
+    // Smooth scroll to top when the button is clicked
+    $('#backToTop').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 100);
+    });
 });
