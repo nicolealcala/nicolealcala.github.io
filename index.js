9d5c070bf514af07038270a31a8588dfdcdbab82
nicolealcala <nicolealcalq@gmail.com>
2023-08-06 20:50:27 +0800

    loader

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    var loader = $("#sectionLoader");
+    $(window).on("load", function () {
+        loader.hide(); // Hide the loader when the page has finished loading
+    });
+});
