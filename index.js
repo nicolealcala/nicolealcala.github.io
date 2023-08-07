58b569b0681c25ed07c7b34364df5c0ffe5e8a2d
nicolealcala <nicolealcalq@gmail.com>
2023-08-07 18:52:58 +0800

    small responsiveness

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-    var loader = $("#sectionLoader");
-    $(window).on("load", function () {
-        loader.hide(); // Hide the loader when the page has finished loading
-    });
-
     var landingPageHeight = $("#landingPage").height();
     $(window).scroll(function () {
         if ($(this).scrollTop() > landingPageHeight) {
