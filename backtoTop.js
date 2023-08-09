6ebfce671845578944e0822397083d57618db4cb
nicolealcala <nicolealcalq@gmail.com>
2023-08-09 10:09:41 +0800

    backToTop separated

--- backtoTop.js
+++ backtoTop.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
+
+    // Smooth scroll to top when the button is clicked
+    $('#backToTop').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 100);
+    });
+
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 500) {
+        if ($(this).scrollTop() > 300) {
             $('#backToTop').addClass("backToTopShow");
         } else {
             $('#backToTop').removeClass("backToTopShow");
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 $(document).ready(function () {
-    // Smooth scroll to top when the button is clicked
-    $('#backToTop').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 100);
-    });
+
 });
