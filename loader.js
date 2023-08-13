da15f2e5cfe4b3cfaec702050ba5779de54d4b95
nicolealcala <nicolealcalq@gmail.com>
2023-08-13 14:03:47 +0800

    small screen links fixed

--- loader.js
+++ loader.js
@@ -2,10 +2,9 @@ $(document).ready(function () {
     var loader = $("#sectionLoader");
 
     function fetchData() {
-        loader.css('display', 'block');
 
         setTimeout(function () {
-            loader.css('display', 'none');
+            loader.fadeOut()
             window.scrollTo(0, 0);
         }, 3000); // Simulate a 3-second delay
     }
