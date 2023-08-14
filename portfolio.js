1cb5fde95cb562abb1c75472f3013b10495cc33d
nicolealcala <nicolealcalq@gmail.com>
2023-08-14 20:51:45 +0800

    linked figma pages (no content)

---
+++ portfolio.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('#allFilter').click(function () {
+        if ($(this).hasClass('active')) {
+            $('.projectCol').show();
+        } else {
+            $('li').not(this).removeClass('active');
+            $(this).toggleClass('active');
+            $('.projectCol').show();
+        }
+    })
+
+    $('#deployedFilter').click(function () {
+        if ($(this).hasClass('active')) {
+            $('.projectCol').not('.deployed').hide();
+            $('.deployed').show();
+        } else {
+            $('li').not(this).removeClass('active');
+            $(this).toggleClass('active');
+            $('.projectCol').not('.deployed').hide();
+            $('.deployed').show();
+        }
+    });
+
+    $('#uiFilter').click(function () {
+        if ($(this).hasClass('active')) {
+            $('.projectCol').not('.ui').hide();
+            $('.ui').show();
+        } else {
+            $('li').not(this).removeClass('active');
+            $(this).toggleClass('active');
+            $('.projectCol').not('.ui').hide();
+            $('.ui').show();
+        }
+    })
+})
