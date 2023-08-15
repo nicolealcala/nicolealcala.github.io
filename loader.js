7b1ed3437888c3f3fde6b28720e9c382728c5597
nicolealcala <nicolealcalq@gmail.com>
2023-08-15 22:43:54 +0800

    loader fixed

--- loader.js
+++ loader.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
     var loader = $("#sectionLoader");
-
-    function fetchData() {
-
-        setTimeout(function () {
-            loader.fadeOut("fast")
-            // window.scrollTo(0, 0);
-        }, 2000); // Simulate a 3-second delay
-    }
-    fetchData();
+    loader.fadeOut("fast");
 })
