70c2b05006a6a383e84ee7ddb837a26ac2520fea
nicolealcala <nicolealcalq@gmail.com>
2023-08-16 19:45:20 +0800

    nav toggle check-icon

--- nav.js
+++ nav.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
-    $("#menuBtn, #check-icon").click(function () {
+    $("#menuBtn").click(function () {
         // Toggle visibility of sections
         $("section").toggleClass('d-none');
 
+        $("#check-icon").prop("checked", !$("#check-icon").prop("checked"));
         // Toggle visibility of .navbar-brand
         $(".navbar-brand").toggleClass('d-none');
         $("#menuBtn").toggleClass('white, darkTxt');
         $(".bar").toggleClass('whiteBg, darkBg');
     });
+
 
 });
