36a16ad60a3c195a7b0bac17080b82028ed5bfc9
nicolealcala <86728391+nicolealcala@users.noreply.github.com>
2024-02-20 13:05:45 +0800

    JM slide

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -34,42 +34,134 @@ $(document).ready(function () {
     });
 })
 
-const projectModal = new bootstrap.Modal(document.getElementById('projectModal'));
-const modalBody = $("#modalBodyImg");
-const modalHeader = $(".modal-title");
-var imgUrls = [];
-var modalImg = '';
+// const projectModal = new bootstrap.Modal(document.getElementById('projectModal'));
+// const modalBody = $("#modalBodyImg");
+// const modalHeader = $(".modal-title");
+// var imgUrls = [];
+// var modalImg = '';
 
-function openModal(project) {
-    modalImg = '';
-    modalHeader[0].innerHTML = "";
-    modalBody[0].innerHTML = modalImg;
-    loadContents(project);
+// function openModal(project) {
+//     modalImg = '';
+//     modalHeader[0].innerHTML = "";
+//     modalBody[0].innerHTML = modalImg;
+//     loadContents(project);
+// }
+
+// function closeModal() {
+//     projectModal.hide();
+// }
+
+// function loadContents(project) {
+//     if (project === 'sws') {
+//         imgUrls = [
+//             "design/sws/thumbnail.png",
+//         ]
+
+//         appendImages(imgUrls, "Strays Worth Saving (SWS) Unofficial Web Design Preview");
+//         modalHeader[0].innerHTML = "Strays Worth Saving (SWS) Unofficial Web Design";
+//         projectModal.show();
+//     } else if (project === "loungescape") {
+//         imgUrls = [
+//             "design/loungescape/thumbnail.png",
+//         ]
+
+//         appendImages(imgUrls, "Loungescape Preview");
+//         modalHeader[0].innerHTML = "Loungescape";
+//         projectModal.show();
+//     } else if (project === "kkis") {
+//         imgUrls = [
+//             "design/kkis/01.png",
+//             "design/kkis/02.png",
+//             "design/kkis/03.png",
+//             "design/kkis/04.png",
+//             "design/kkis/05.png",
+//             "design/kkis/06.png",
+//             "design/kkis/07.png",
+//             "design/kkis/08.png",
+//             "design/kkis/09.png",
+//             "design/kkis/10.png",
+//             "design/kkis/11.png",
+//             "design/kkis/12.png",
+//             "design/kkis/13.png",
+//             "design/kkis/14.png",
+//             "design/kkis/15.png"
+//         ]
+
+//         appendImages(imgUrls, "KKIS Preview");
+//         modalHeader[0].innerHTML = "Katipunan ng Kabataan Information System (KKIS)";
+//         projectModal.show();
+//     } else if (project === "unblokc") {
+//         imgUrls = [
+//             "design/unblokc/thumbnail.png",
+//         ]
+
+//         appendImages(imgUrls, "UNBLOKC Hackathon Website Preview");
+//         modalHeader[0].innerHTML = "UNBLOKC Hackathon Website";
+//         projectModal.show();
+//     } else if (project === "medeco") {
+//         imgUrls = [
+//             "design/medeco/thumbnail.png",
+//             "design/medeco/01.png",
+//             "design/medeco/02.png",
+//             "design/medeco/03.png",
+
+//         ]
+//         appendImages(imgUrls, "Medeco Preview");
+//         modalBody[0].innerHTML = modalImg;
+//         modalHeader[0].innerHTML = "Medeco";
+//         projectModal.show();
+//     } else if (project === "portfolio") {
+//         imgUrls = [
+//             "design/portfolio/01.png",
+//             "design/portfolio/02.png",
+//             "design/portfolio/03.png",
+//             "design/portfolio/04.png"
+//         ]
+
+//         appendImages(imgUrls, "Portfolio Website Preview");
+//         modalHeader[0].innerHTML = "Portfolio Website";
+//         projectModal.show();
+//     } else if (project === "gradInvi") {
+//         imgUrls = [
+//             "design/gradInvi/thumbnail.png",
+//             "design/gradInvi/01.png",
+//             "design/gradInvi/02.png",
+//         ]
+
+//         appendImages(imgUrls, "Class of 2023 Graduation Invitation Design Concept Preview");
+//         modalHeader[0].innerHTML = "Class of 2023 Graduation Invitation Design Concept";
+//         projectModal.show();
+//     }
+// }
+
+// function appendImages(imgSet, imgAlt) {
+//     imgSet.forEach(url => modalImg += '<img src="' + url + '" class="modalImg" alt="' + imgAlt + '">');
+//     modalBody[0].innerHTML = modalImg;
+// }
+
+//Carousel 
+const carouselInner = $("#carousel");
+var imgSrcs = [];
+
+function openCarousel(project) {
+    loadCarouselSlides(project)
 }
 
-function closeModal() {
-    projectModal.hide();
-}
-
-function loadContents(project) {
+function loadCarouselSlides(project) {
     if (project === 'sws') {
-        imgUrls = [
+        imgSrcs = [
             "design/sws/thumbnail.png",
         ]
 
-        appendImages(imgUrls, "Strays Worth Saving (SWS) Unofficial Web Design Preview");
-        modalHeader[0].innerHTML = "Strays Worth Saving (SWS) Unofficial Web Design";
-        projectModal.show();
+        createCarouselSlide(imgSrcs, "Strays Worth Saving (SWS) Unofficial Web Design Preview");
     } else if (project === "loungescape") {
-        imgUrls = [
+        imgSrcs = [
             "design/loungescape/thumbnail.png",
         ]
 
-        appendImages(imgUrls, "Loungescape Preview");
-        modalHeader[0].innerHTML = "Loungescape";
-        projectModal.show();
+        createCarouselSlide(imgSrcs, "Loungescape Preview");
     } else if (project === "kkis") {
-        imgUrls = [
+        imgSrcs = [
             "design/kkis/01.png",
             "design/kkis/02.png",
             "design/kkis/03.png",
@@ -87,54 +179,75 @@ function loadContents(project) {
             "design/kkis/15.png"
         ]
 
-        appendImages(imgUrls, "KKIS Preview");
-        modalHeader[0].innerHTML = "Katipunan ng Kabataan Information System (KKIS)";
-        projectModal.show();
+        createCarouselSlide(imgSrcs, "KKIS Preview");
     } else if (project === "unblokc") {
-        imgUrls = [
+        imgSrcs = [
             "design/unblokc/thumbnail.png",
         ]
 
-        appendImages(imgUrls, "UNBLOKC Hackathon Website Preview");
-        modalHeader[0].innerHTML = "UNBLOKC Hackathon Website";
-        projectModal.show();
+        createCarouselSlide(imgSrcs, "UNBLOKC Hackathon Website Preview");
     } else if (project === "medeco") {
-        imgUrls = [
+        imgSrcs = [
             "design/medeco/thumbnail.png",
             "design/medeco/01.png",
             "design/medeco/02.png",
             "design/medeco/03.png",
-
         ]
-        appendImages(imgUrls, "Medeco Preview");
-        modalBody[0].innerHTML = modalImg;
-        modalHeader[0].innerHTML = "Medeco";
-        projectModal.show();
+
+        createCarouselSlide(imgSrcs, "Medeco Preview");
     } else if (project === "portfolio") {
-        imgUrls = [
+        imgSrcs = [
             "design/portfolio/01.png",
             "design/portfolio/02.png",
             "design/portfolio/03.png",
             "design/portfolio/04.png"
         ]
 
-        appendImages(imgUrls, "Portfolio Website Preview");
-        modalHeader[0].innerHTML = "Portfolio Website";
-        projectModal.show();
+        createCarouselSlide(imgSrcs, "Portfolio Website Preview");
     } else if (project === "gradInvi") {
-        imgUrls = [
+        imgSrcs = [
             "design/gradInvi/thumbnail.png",
             "design/gradInvi/01.png",
             "design/gradInvi/02.png",
         ]
 
-        appendImages(imgUrls, "Class of 2023 Graduation Invitation Design Concept Preview");
-        modalHeader[0].innerHTML = "Class of 2023 Graduation Invitation Design Concept";
-        projectModal.show();
+        createCarouselSlide(imgSrcs, "Class of 2023 Graduation Invitation Design Concept Preview");
     }
 }
 
-function appendImages(imgSet, imgAlt) {
-    imgSet.forEach(url => modalImg += '<img src="' + url + '" class="modalImg" alt="' + imgAlt + '">');
-    modalBody[0].innerHTML = modalImg;
+function createCarouselSlide(imgSrcs, imgAlt) {
+    var counter = 1;
+
+    imgSrcs.forEach((imgSrc) => {
+        //Indicator
+        var carouselIndicatorRow = $("#carouselIndicatorRow");
+        var buttonIndicator = document.createElement("button");
+        buttonIndicator.type = 'button';
+        buttonIndicator.dataset.bsTarget = '#projectCarousel';
+        buttonIndicator.dataset.bsSlideTo = counter - 1;
+        buttonIndicator.classList.add('active');
+        buttonIndicator.setAttribute('aria-current', 'true');
+        buttonIndicator.setAttribute('aria-label', 'Slide ' + counter);
+
+        carouselIndicatorRow.append(buttonIndicator);
+
+        //Slide Div
+        var slideDiv = document.createElement("div");
+        slideDiv.classList.add("carousel-item");
+        if (counter === 1) {
+            slideDiv.classList.add("active");
+        }
+
+        //Slide Img
+        var slideImg = document.createElement("img");
+        slideImg.src = imgSrc;
+        slideImg.alt = imgAlt;
+        slideImg.classList.add("d-block w-100 modalImg");
+
+        slideDiv.appendChild(slideImg);
+
+        $("#carouselInner").append(slideDiv);
+        counter++;
+        $('#projectCarousel').carousel();
+    });
 }
